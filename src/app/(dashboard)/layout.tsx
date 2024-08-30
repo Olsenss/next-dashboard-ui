3e3bccd478424b0719384b58faa5821d700f4c6e
Martin Winberg Olsen <MWO@andel.dk>
2024-08-30 12:27:50 +0200

    chore: Add dashboard pages for admin, parent, student, and teacher

---
+++ src/app/(dashboard)/layout.tsx
@@ -0,0 +1,19 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Lama Dev School Management Dashboard",
+  description: "Next.js School Management System",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div>dashboard{children}</div>
+  );
+}
